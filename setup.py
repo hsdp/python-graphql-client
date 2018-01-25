64b0679ea8fa827ab11ffe06040d54650672af42
ksmithson <ksmithson@hsdp.io>
2018-01-25 13:36:57 -0800

    added rst readme

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="graphql_client",
     description="A GraphQL Client",
+    long_description=open('README.rst').read(),
     version="0.1",
     packages=find_packages(),
     install_requires=[
