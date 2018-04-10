12d7cb0dafb0319610b4f518550bd01b0cbff717
ksmithson <ksmithson@hsdp.io>
2018-04-10 10:28:14 -0700

    add operation_name and extra data for future use.

--- graphql_client/client.py
+++ graphql_client/client.py
@@ -11,23 +11,29 @@ class GraphQLClient(object):
         self.endpoint = endpoint
         self.headers = headers;
 
-    def query(self, query: str, variables: Optional[Dict[str, Any]]=None):
-        return self.send(query, variables)
+    def query(self, query: str, variables: Optional[Dict[str, Any]]=None, operation_name: Optional[str]=None):
+        return self.send(query, variables, operation_name=operation_name)
 
-    def query_from_file(self, file, variables: Optional[Dict[str, Any]]=None):
+    def query_from_file(self, file, variables: Optional[Dict[str, Any]]=None, operation_name: Optional[str]=None):
         with open(file) as query:
-            return self.send(query.read(), variables)
+            return self.send(query.read(), variables, operation_name=operation_name)
 
     def send(
         self,
         query: str,
         variables: Optional[Dict[str, Any]]=None,
-        extra_headers: Optional[Dict[str, Any]]=None
+        extra_headers: Optional[Dict[str, Any]]=None,
+        operation_name: Optional[str]=None,
+        extra_data: Optional[Dict[str, Any]]=None
     ):
         data = {
             'query': query,
-            'variables': variables
+            'variables': variables,
+            'operationName': operation_name
         }
+
+        if extra_data:
+            data.update(extra_data)
 
         headers = {
             'Accept': 'application/json',
